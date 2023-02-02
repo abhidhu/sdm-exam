4dc82daa4d510874e3011a19105f00e073fb5466
Abhi <abhijeetdhumal118981@gmail.com>
2023-02-02 15:35:23 +0530

    third commit

--- Fabbo.java
+++ Fabbo.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
 
 public class Fabbo{
+
+    static int fabbo(int n){
+        if(n==0 || n==1) return 1;
+
+        return fabbo(n-1)+fabbo(n-2);
+    }
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         // System.out.println("Enter a number");
         // int n=scanner.nextInt();
         int n=20;
+        for(int i=0; i<n; i++)
+         System.out.println(fabbo(i));
+        
+
     }
 }
