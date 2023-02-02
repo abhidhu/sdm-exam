9b95b5df5bdd1f0b0e6c6fa2609d842d11e6ffc1
Abhi <abhijeetdhumal118981@gmail.com>
2023-02-02 15:40:26 +0530

    forth commit

--- Fabbo.java
+++ Fabbo.java
@@ -7,14 +7,23 @@ public class Fabbo{
 
         return fabbo(n-1)+fabbo(n-2);
     }
+    static boolean isPrime(int n){
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0)  return false;
+        }
+        return true;
+    }
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         // System.out.println("Enter a number");
         // int n=scanner.nextInt();
         int n=20;
-        for(int i=0; i<n; i++)
-         System.out.println(fabbo(i));
+        // for(int i=0; i<n; i++)
+        //  System.out.println(fabbo(i));
         
+        for(int i=1; i<=n;i++){
+            if(isPrime(i)) System.out.println(i);
+        }
 
     }
 }
