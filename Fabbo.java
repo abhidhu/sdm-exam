625c290647c4fc2b3e5f3b862650efac198ee6a9
Abhi <abhijeetdhumal118981@gmail.com>
2023-02-02 15:29:46 +0530

    secound commit

---
+++ Fabbo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Fabbo{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        // System.out.println("Enter a number");
+        // int n=scanner.nextInt();
+        int n=20;
+    }
+}
